578023aa8e0df26eeeb105778e9972ce1f994c0e
Aziz Ayari <azizayari879@gmail.com>
2025-04-23 18:12:02 +0100

    second problem

---
+++ chapter-1/problem-2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // directve
+
+int main(void)
+{
+	printf("Parkinson's Law: \nWork expands so as to "); // statement 1
+	printf("fill the time\n"); // statement 2
+	printf("available for its completion. \n"); // statemetn 3
+	return 0; // statement 4
+}
+
+// Output:
+//
+// Parkinson's Law:
+// Work expands so as tofill the time
+// available for its completion.
